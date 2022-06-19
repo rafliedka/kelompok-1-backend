e67fc58d2b774aa8912d92b4fad7dcec759434b4
ryasakbar060 <ryasakbar060@gmail.com>
2022-06-19 21:05:58 +0800

    add system login in user controller with jwt

--- controller/user.js
+++ controller/user.js
@@ -1,5 +1,6 @@
 const { user } = require('../models');
 const bcrypt = require('bcrypt');
+const jwt = require('../helper/jwt');
 
 module.exports = class {
   // Register
@@ -31,5 +32,53 @@ module.exports = class {
       .catch((err) => {
         res.status(400).send(err);
       });
+  }
+
+  // Login
+  static async loginUser(req, res, next) {
+    try {
+      // check user with email
+      const users = await user.findOne({
+        where: {
+          email: req.body.email,
+        },
+      });
+      if (!users) {
+        res.status(404).send({
+          status: 404,
+          message: 'User Not Found',
+        });
+      }
+
+      // check user with password
+      const isValidPassword = await bcrypt.compare(req.body.password, users.password);
+      // console.log(isValidPassword);
+      if (!isValidPassword) {
+        res.status(400).send({
+          status: 400,
+          message: 'Email and Password Not Match',
+        });
+      }
+
+      // generate token user with jwt
+      const token = jwt.generateToken({
+        email: users.email,
+        password: users.password,
+      });
+
+      const secureUser = users.dataValues;
+      delete secureUser.password;
+
+      res.status(200).send({
+        status: 200,
+        message: 'User Found',
+        data: {
+          users: secureUser,
+          token: token,
+        },
+      });
+    } catch (error) {
+      res.status(404).send(error);
+    }
   }
 };
