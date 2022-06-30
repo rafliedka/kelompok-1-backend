3fe38f5589f347a262ee56a0f45fd0aba1dd96e5
ryasakbar060 <ryasakbar060@gmail.com>
2022-06-30 20:37:49 +0800

    add multer

--- controller/product.js
+++ controller/product.js
@@ -1,68 +1,89 @@
-const { product } = require('../models')
+const { product } = require('../models');
 
 module.exports = class {
-  static async getAllProduct (req, res) {
-    const result = await product.findAll()
+  static async getAllProduct(req, res) {
+    const result = await product.findAll();
     try {
       res.status(200).json({
         status: 200,
-        data: result
-      })
+        data: result,
+      });
     } catch (error) {
-      res.status(400).send(error)
+      res.status(400).send(error);
     }
   }
 
-  static async getProductById (req, res) {
-    const result = await product.findOne({ where: { id: req.params.id } })
+  static async getProductById(req, res) {
+    const result = await product.findOne({ where: { id: req.params.id } });
     try {
       res.status(200).json({
         status: 200,
-        data: result
-      })
+        data: result,
+      });
     } catch (error) {
-      res.send(error)
+      res.send(error);
     }
   }
 
-  static async addProduct (req, res) {
-    const result = await product.create(req.body)
-    try {
-      res.status(201).json({
-        status: 201,
-        message: 'product has been added',
-        data: result
+  static async addProduct(req, res) {
+    product
+      .create({
+        name: req.body.name,
+        image: req.file.path,
+        price: req.body.price,
+        category: req.body.category,
+        description: req.body.description,
+        user_id: req.body.user_id,
       })
-    } catch (error) {
-      res.status(400).send(error)
-    }
+      .then((result) => {
+        res.status(201).json({
+          status: 201,
+          message: 'product has been added',
+          data: result,
+        });
+      })
+      .catch((err) => {
+        res.status(400).send(err);
+      });
   }
 
-  static async updateProduct (req, res) {
-    await product.update({ ...req.body }, { where: { id: req.params.id } })
-    try {
-      res.status(201).json({
-        status: 201,
-        message: 'car data has been update',
-        data: req.body
+  static async updateProduct(req, res) {
+    await product
+      .update(
+        {
+          ...req.body,
+        },
+        {
+          where: {
+            id: req.params.id,
+          },
+          returning: true,
+        }
+      )
+      .then((result) => {
+        res.status(201).json({
+          status: 201,
+          message: 'product has been update',
+          data: result,
+        });
       })
-    } catch (error) {
-      res.status(400).send(error)
-    }
+      .catch((err) => {
+        res.status(400).send(err);
+      });
   }
 
-  static async deleteProduct (req, res) {
-    await product.destroy({ where: { id: req.params.id } })
+  static async deleteProduct(req, res) {
+    await product.destroy({ where: { id: req.params.id } });
     try {
       res.status(204).json({
         staus: 204,
-        message: 'product has been deleted'
-      })
+        message: 'product has been deleted',
+      });
     } catch (error) {
       res.status(422).json({
         status: 422,
-        message: error.message
-      })
+        message: error.message,
+      });
     }
   }
-}
+};
