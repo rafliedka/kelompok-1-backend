20a566359ef604c4a8292e4fb2ebad8dabdba96a
Rafli Edka <rafliedka33@gmail.com>
2022-07-18 06:04:23 +0100

    add cloudinary

--- helper/multer.js
+++ helper/multer.js
@@ -1,15 +1,16 @@
+// Multer User
 const multer = require('multer')
 const path = require('path')
 
 const storage = multer.diskStorage({
-  destination: function(req, file, cb) {
-    cb(null, 'public/photoProduct')
+  destination: function (req, file, cb) {
+    cb(null, 'public/photoUser')
   },
-  filename: function(req, file, cb) {
+  filename: function (req, file, cb) {
     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9)
     cb(null, file.fieldname + '-' + uniqueSuffix + path.extname(file.originalname))
   }
 })
-const upload = multer({
-  storage
-})
+const upload = multer({ storage })
+
+module.exports = upload
--- routes/product.js
+++ routes/product.js
@@ -3,32 +3,25 @@ const router = express.Router()
 const ProductController = require('../controller/product')
 const sellerAuth = require('../middleware/isSeller')
 
-// Multer Product
+const cloudinary = require('cloudinary').v2
+const { CloudinaryStorage } = require('multer-storage-cloudinary')
 const multer = require('multer')
-const path = require('path')
 
-const storage = multer.diskStorage({
-  destination: function(req, file, cb) {
-    cb(null, 'public/photoProduct')
-  },
-  filename: function(req, file, cb) {
-    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9)
-    cb(null, file.fieldname + '-' + uniqueSuffix + path.extname(file.originalname))
+const storage = new CloudinaryStorage({
+  cloudinary,
+  params: {
+    folder: 'product',
+    format: async (req, file) => 'jpg', // supports promises as well
+    public_id: (req, file) => req.params.id
   }
 })
-const upload = multer({
-  storage
-})
 
-/* GET users listing. */
-// router.get('/', function(req, res, next) {
-//   res.send('respond with a resource');
-// });
+const parser = multer({ storage })
 
 router.get('/', ProductController.getAllProduct)
 router.get('/:id', sellerAuth, ProductController.getProductById)
-router.post('/', sellerAuth, upload.single('image'), ProductController.addProduct)
-router.put('/:id', sellerAuth, upload.single('image'), ProductController.updateProduct)
+router.post('/', sellerAuth, parser.single('image'), ProductController.addProduct)
+router.put('/:id', sellerAuth, parser.single('image'), ProductController.updateProduct)
 router.delete('/:id', sellerAuth, ProductController.deleteProduct)
 
 module.exports = router
