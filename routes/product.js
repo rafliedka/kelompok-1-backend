4a93586bfdcd060243a19c1f3fec83144360f972
Rafli Edka <rafliedka33@gmail.com>
2022-07-20 07:46:02 +0100

    test

--- routes/product.js
+++ routes/product.js
@@ -3,7 +3,22 @@ const router = express.Router()
 const ProductController = require('../controller/product')
 const sellerAuth = require('../middleware/isSeller')
 
-// Multer Product
+/* const cloudinary = require('cloudinary').v2
+const { CloudinaryStorage } = require('multer-storage-cloudinary')
+const multer = require('multer')
+
+const storage = new CloudinaryStorage({
+  cloudinary,
+  params: {
+    folder: 'product',
+    format: async (req, file) => 'jpg', // supports promises as well
+    public_id: (req, file) => req.params.id
+  }
+})
+
+const parser = multer({ storage }) */
+
+// Multer User
 const multer = require('multer')
 const path = require('path')
 
@@ -16,14 +31,9 @@ const storage = multer.diskStorage({
     cb(null, file.fieldname + '-' + uniqueSuffix + path.extname(file.originalname))
   }
 })
-const upload = multer({
-  storage
-})
+const upload = multer({ storage })
 
-/* GET users listing. */
-// router.get('/', function(req, res, next) {
-//   res.send('respond with a resource');
-// });
+module.exports = upload
 
 router.get('/', ProductController.getAllProduct)
 router.get('/:id', sellerAuth, ProductController.getProductById)
--- routes/user.js
+++ routes/user.js
@@ -3,6 +3,24 @@ const router = express.Router()
 const UserController = require('../controller/user')
 const auth = require('../middleware/authentication')
 
+// cloudinary
+/* const cloudinary = require('cloudinary').v2
+const { CloudinaryStorage } = require('multer-storage-cloudinary')
+const multer = require('multer')
+
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: {
+    folder: (req, file) => 'users',
+    format: async (req, file) => {
+      return 'jpeg'
+    },
+    public_id: (req, file) => req.params.id
+  },
+});
+
+const parser = multer({ storage }) */
+
 // Multer User
 const multer = require('multer')
 const path = require('path')
@@ -18,10 +36,7 @@ const storage = multer.diskStorage({
 })
 const upload = multer({ storage })
 
-/* GET users listing. */
-// router.get('/', function(req, res, next) {
-//   res.send('respond with a resource');
-// });
+module.exports = upload
 
 router.get('/', UserController.getAllUser)
 router.get('/:id', auth, UserController.fetchUserId)
