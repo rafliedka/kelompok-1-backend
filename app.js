4709c9b4e93199863d9e3263e3e3f659d3af5ead
Rafli Edka <rafliedka33@gmail.com>
2022-07-20 15:15:33 +0100

    add cors

--- app.js
+++ app.js
@@ -3,11 +3,14 @@ const express = require('express')
 const path = require('path')
 const cookieParser = require('cookie-parser')
 const logger = require('morgan')
+const cors = require('cors')
 
 const indexRouter = require('./routes/index')
 // var usersRouter = require('./routes/users');
 
 const app = express()
+
+app.use(cors())
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'))
